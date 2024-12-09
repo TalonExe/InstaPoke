14dc07d9ef51fde2e8a6fe6c3fd5339470234b5a
root <root@DESKTOP-6AB893J.>
2024-12-09 23:21:04 +0800

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+let i = $state(0)
+
+</script>
+
+<div class="bg-black w-screen h-screen text-white flex justify-center align-center">
+    <div>
+    <h1>Welcome to SvelteKit</h1>
+    <p>{i}</p>
+    <button onclick={() => i++}>Increment</button>
+    </div>
+</div>
